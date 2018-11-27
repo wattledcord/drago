b81f3767de13491628d638d655e1d7cbc360c7b1
B Madhava Reddy <bmadhavreddy2015@gmail.com>
2018-11-27 07:00:19 +0530

    Added Material UI support and axios library for API calling

--- src/client/app.js
+++ src/client/app.js
@@ -1,6 +1,25 @@
-import React ,{Component} from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import TextField from '@material-ui/core/TextField';
 export default class App extends Component {
-    render(){
-        return <div>Hi I Am Madhav  The greatest developer of ll time </div>
+    constructor(props) {
+        super(props)
+        axios.get('/employees').then((response) => {
+            console.log(response.data);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+    render() {
+        return <form>
+            <TextField id="EmployeeName" label="Employee Name" fullWidth margin="normal"  variant="outlined"></TextField>
+            <TextField id="FirstName" label="FirstName" fullWidth margin="normal"  variant="outlined"></TextField>
+            <TextField id="LastName" label="LastName" fullWidth margin="normal"  variant="outlined"></TextField>
+            <TextField id="PhoneNumber" label="PhoneNumber" type="number" fullWidth margin="normal"  variant="outlined"></TextField>
+            <TextField id="Address" label="Address" multiline rowsMax="4" margin="normal" fullWidth  variant="outlined"></TextField>
+            <TextField id="Company" label="Company" fullWidth margin="normal"  variant="outlined"></TextField>
+            <TextField id="Designation" label="Designation" fullWidth margin="normal"  variant="outlined"></TextField>
+            <TextField id="HighestEducation" label="HighestEducation" fullWidth margin="normal"  variant="outlined"> </TextField>
+        </form>
     }
 }
